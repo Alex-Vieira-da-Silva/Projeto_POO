85d79f74d2771b0c1b91531eef39b4677b63461a
alex <alexvieira.360@hotmail.com>
2024-07-09 21:19:35 -0300

    commit projeto finalizado

---
+++ POO - Desafio/iPhone/src/com/apple/app/Aparelho.java
@@ -0,0 +1,13 @@
+package com.apple.app;
+
+import com.apple.model.Iphone;
+
+public class Aparelho {
+
+	public static void main(String[] args) {
+		
+		Iphone iPhone = new Iphone();
+	
+		iPhone.home();		
+	}
+}
